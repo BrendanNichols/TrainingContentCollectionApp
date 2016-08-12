67b0a799c88db43cfd2a525b0917f838a5c56b03
Brendan Nichols <BrendanNichols@beckgroup.com>
2016-08-12 14:57:31 -0400

    working uploads

---
+++ TrainingContentCollectionApp/obj/Release/Package/PackageTmp/scripts/App/Main.Controller.js
@@ -0,0 +1,98 @@
+﻿
+
+
+angular.module('app')
+    .controller('mainController', mainController);
+
+
+mainController.$inject = ['$scope', '$stateParams','$state','Main','TypesFactory'];
+
+function mainController($scope, $stateParams, $state, Main, TypesFactory) {
+    console.log("in mainController");
+
+    $scope.goToFormState = function (page) {
+        console.log("here is page:")
+        console.log(page.Id)
+
+        $state.go('home.form', { Page: page.Id })
+        
+    }
+
+    Main.getPages().then(function (response) {    
+        $scope.allPages = response.data;
+        console.log($scope.allPages[0].Name);
+        for (var i = 0; i < $scope.allPages.length; i++) {
+            $scope.allPages[i].ParsedName = $scope.allPages[i].Name.split('.')[0].split(/(?=[A-Z])/).join(" ");
+            //console.log($scope.allPages[i].ParsedName);
+        }
+    })
+
+    TypesFactory.getTypes().then(function (response) {
+        $scope.pageTypes = response.data;
+        console.log($scope.pageTypes);
+    })
+    
+    $scope.typeSelect = function (selectedType) {
+        console.log('in typeSelect' + selectedType.Id);
+        $scope.currentPage = [];
+        $scope.pages = [];
+        for (i = 0; i < $scope.allPages.length; i++) {
+            if ($scope.allPages[i].PageTypeId == selectedType.Id) {
+                $scope.pages.push($scope.allPages[i]);
+            };
+        };
+    };
+
+    $scope.selectedPage = function (selectedPage) {
+        console.log(selectedPage.Name);
+        $scope.currentPage = selectedPage;
+    }
+
+
+    //dropdown shit
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function (open) {
+        $log.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+};
+
+
+
+
+
+
+
+
+    //, ['PageFactory', function (PageFactory) {
+        
+    //        var vm = this;
+    //        vm.food = 'pizza';
+    //        vm.pages = [];
+
+    //        activate();
+
+    //        function activate() {
+    //            getPages();
+    //        }
+            
+
+    //        function getPages() {
+    //            PageFactory.getPages()
+    //                .then(function (response) {
+    //                    vm.pages = response.data;
+    //                })
+    //            //.success(vm.pages = data)
+    //            return vm.pages;
+    //        }
+        
+    //}]);
---
+++ TrainingContentCollectionApp/obj/Release/Package/PackageTmp/scripts/App/Main.module.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    var app = angular.module('app', ['ngResource', 'ui.router', 'mainServices','ui.bootstrap']);
+    console.log("in main module");
+    app.config(['$stateProvider', '$urlRouterProvider', '$resourceProvider', function ($stateProvider, $urlRouterProvider, $resourceProvider) {
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home.html',
+                controller: 'mainController'
+            })
+            .state('home.form', {
+                url: 'form/:Page',
+                templateUrl: 'form.html',
+                controller: 'formController'
+            })
+
+    }])
+    //uploading file directive
+    app.directive('ngFiles', ['$parse', function ($parse) {
+
+        function fn_link(scope, element, attrs) {
+            var onChange = $parse(attrs.ngFiles);
+            element.on('change', function (event) {
+                onChange(scope, { $files: event.target.files });
+            });
+        };
+
+        return {
+            link: fn_link
+        }
+    }])
+})();
